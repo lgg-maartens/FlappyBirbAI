4991eafa6fe32d6d00bd228719a1ebb6bad7fc03
Tim Logtenberg <45438244+lgg-maartens@users.noreply.github.com>
2021-09-29 10:53:42 +0000

    rotate bird on jump/fall

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-var y;
-var velocity;
-var acceleration;
-var gravity;
+var rects = [];
+var birb;
+var bg, birb_img;
 
 class Birb {
   constructor() {
-    this.x;
+    this.x = 640 / 2;
     this.y = 100;
     this.velocity = 0;
     this.acceleration = 0.9;
@@ -13,6 +12,7 @@ class Birb {
   }
 
   draw() {
+
     if (this.y >= height) {
       this.y = height;
 
@@ -25,8 +25,20 @@ class Birb {
       this.y += this.velocity;
     }
 
-    fill("red");
-    ellipse(640 / 2, this.y, 25, 25);
+    // rotation
+    translate(this.x, this.y);     
+    if (this.velocity > 0) {
+      rotate(10);
+      image(birb_bg, -30, -15, 60, 30);
+      rotate(-10);
+    } else {
+      rotate(-10);
+      image(birb_bg, -30, -15, 60, 30);
+      rotate(10);
+    }
+    translate(-this.x,-this.y);
+
+   
   }
 }
 
@@ -41,44 +53,43 @@ class Pipe {
   }
 
   drawRect() {
-    fill(this.color)
-    rect(this.x, this.y, this.w, this.h);
+    fill(this.color);
     this.x = this.x + this.vx;
+    rect(this.x, this.y, this.w, this.h);
   }
 }
 
-var rects = [];
-var birb;
-var bg;
+
 
 function preload() {
   bg = loadImage('bg.png');
+  birb_bg = loadImage('birb.png');
 }
 
 function setup() {
   createCanvas(640, 360);
+  angleMode(DEGREES);
 
   birb = new Birb();
 }
 
 function draw() {
-  image(bg, 0,0, width, height);
+  image(bg, 0, 0, width, height);
 
 
   if (frameCount % 60 == 0) {
     console.log(frameCount);
 
-    let randHeight = random(height/2);
+    let randHeight = random(height / 2);
     let gapHeight = 150;
 
-    
-    let newRectTop = new Pipe(640, 0, 30, randHeight, -5, "green");
-    let newRectBot = new Pipe(640, randHeight + gapHeight, 30, height + (randHeight + gapHeight), -5, "green");
+
+    let newRectTop = new Pipe(640, 0, 60, randHeight, -5, "green");
+    let newRectBot = new Pipe(640, randHeight + gapHeight, 60, height + (randHeight + gapHeight), -5, "green");
 
 
     rects.push(newRectBot);
     rects.push(newRectTop);
-    console.log(rects.length)
   }
 
   birb.draw();
